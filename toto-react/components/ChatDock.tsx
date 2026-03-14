e9ad7cdaea5c75a322cdf59107651c203cb3703c
nicolasances <nicolas.matteazzi@gmail.com>
2026-03-14 19:02:51 +0100

    Revert to standard chat behaviour

--- toto-react/components/ChatDock.tsx
+++ toto-react/components/ChatDock.tsx
@@ -304,7 +304,7 @@ export function ChatDock({ sendMessage, streamConversationStatus, onHeightChange
 
       {/* Waiting indicator */}
       {showWaitingIndicator && (
-          <WaitingIndicator />
+        <WaitingIndicator />
       )}
 
       {/* Chat input */}
